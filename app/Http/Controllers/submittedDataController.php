f71b1572712dd4b5fab151ae8b895e4f28c4b6cd
Babak <fakhimelec@gmail.com>
2023-09-08 23:57:42 +0330

    We Used The Model We Created (MemberData) To Connect To The Database. Now If We Input Fields And Press 'Add' Button On 'members' Page, A New Row Will Be Added To The 'member_data' Table

--- app/Http/Controllers/submittedDataController.php
+++ app/Http/Controllers/submittedDataController.php
@@ -4,19 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\MemberData;
+
 class submittedDataController extends Controller
 {
     public function homePage(){
-        
         return view('members');
     }
 
     public function addMember(Request $request) {
         
-        echo $request->post('member_fname');
-        echo $request->post('member_lname');
-        echo $request->post('member_age');
+        $new_mem = new MemberData;
+        $new_mem->fname = $request->post('member_fname');
+        $new_mem->lname = $request->post('member_lname');
+        $new_mem->age = $request->post('member_age');
+        $new_mem->save();
 
-        return view('members');
+        return redirect('/');
     }
 }
